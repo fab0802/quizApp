a4cafe6ccc86a9d21571b411b211bdaccd23e713
Fabian <fabian.gretsch@gmail.com>
2022-12-15 22:24:23 +0100

    start screen created and styled, questions created

---
+++ scripts/quiz.js
@@ -0,0 +1,51 @@
+const quiz = [
+  {
+    question: "What does HTML stand for?",
+    answers: [
+      "Hyperlinks and Text Markup Language",
+      "Hyper Text Markup Language",
+      "Home Tool Markup Language",
+    ],
+    correctAnswer: 1,
+    category: "html",
+  },
+  {
+    question: "Choose the correct HTML element for the largest heading:",
+    answers: ["<h1>", "<heading>", "<h6>"],
+    correctAnswer: 0,
+    category: "html",
+  },
+  {
+    question: "What does CSS stand for?",
+    answers: [
+      "Cascading Style Sheet",
+      "Computer Styling System",
+      "Computer Style Sheet",
+    ],
+    correctAnswer: 0,
+    category: "css",
+  },
+  {
+    question:
+      "What is the correct HTML for referring to an external style sheet?",
+    answers: [
+      '<style src="mystyle.css"',
+      '<link rel="stylesheet" type="text/css" href="mystyle.css">',
+      "<stylesheet>mystyle.css</stylesheet>",
+    ],
+    correctAnswer: 1,
+    category: "css",
+  },
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: ["<js>", "<javascript>", "<script>"],
+    correctAnswer: 2,
+    category: "javascript",
+  },
+  {
+    question: "Where is the correct place to insert a JavaScript?",
+    answers: ["The <body> section", "The <head> Section", "Both"],
+    correctAnswer: 2,
+    category: "javascript",
+  },
+];
