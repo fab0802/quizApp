7e3abcaaf111cfff737f377decfbaddf047b3bac
Fabian <fabian.gretsch@gmail.com>
2022-12-16 00:25:20 +0100

    quiz screen modified

--- script.js
+++ script.js
@@ -3,10 +3,16 @@
 const startButton = document.querySelector("#start-button");
 const startScreen = document.querySelector("#start-screen");
 const questionScreen = document.querySelector("#question-screen");
+let currentQuestion = 0;
+
+startButton.addEventListener("click", intiQuiz);
 
 function intiQuiz() {
   startScreen.classList.add("display-none");
   questionScreen.classList.remove("display-none");
 }
 
-startButton.addEventListener("click", intiQuiz);
+//TODO create renderQuestion function
+function renderQuestion() {
+  console.log();
+}
